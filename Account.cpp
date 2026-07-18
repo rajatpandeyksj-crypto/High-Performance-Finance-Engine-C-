554921be56ce85869ee05eada823cb02cdefe7d8
rajat pandey <rajatpandeyksj@gmail.com>
2026-07-18 23:36:43 +0530

    Update Account.cpp

--- Account.cpp
+++ Account.cpp
@@ -1,1 +1,14 @@
-#include 
+#include "Account.h"
+
+Account :: Account(int id, string name, long long balance){
+
+  this ->id = id;
+  this ->name = name;
+  this ->balance = balance;
+  
+}
+
+void Account::deposit(long long amount){
+balance+=amount;
+
+}
