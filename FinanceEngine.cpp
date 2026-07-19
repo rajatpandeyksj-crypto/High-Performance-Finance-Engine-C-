b5834a00dd59d0e5fe47837ad6caf1de0442af96
rajat pandey <rajatpandeyksj@gmail.com>
2026-07-19 15:02:53 +0530

    Implement start method for FinanceEngine

---
+++ FinanceEngine.cpp
@@ -0,0 +1,17 @@
+void FinanceEngine::start()
+{
+    int choice;
+
+    while(true)
+    {
+        cout<<"1.Create Account\n";
+        cout<<"2.Deposit\n";
+        cout<<"3.Withdraw\n";
+        cout<<"4.Transfer\n";
+        cout<<"5.Exit\n";
+
+        cin>>choice;
+
+        // switch-case
+    }
+}
