7d1b6cf9e29e245ef2e0cb83c20112fd335c462c
rajat pandey <rajatpandeyksj@gmail.com>
2026-07-19 16:59:19 +0530

    Update Wallet.cpp

--- Wallet.cpp
+++ Wallet.cpp
@@ -1,9 +1,42 @@
 void Wallet::createAccount()
 {
-    // account create logic
+   int id;
+    string name;
+    long long balance;
+
+    cout <<"Enter Id: ";
+    cin>>id;
+
+    cout <<"Enter Name: ";
+    cin>>name;
+
+    cout <<"Enter balance: ";
+    cin>>balance;
+
+account acc(id, name, balance);
+    account[id] = acc;
 }
 
-void Wallet::transferMoney()
+
+    void Wallet::transferMoney()
 {
-    // transfer logic
+    int from, to;
+    long long amount;
+
+    cin >> from;
+    cin >> to;
+    cin >> amount;
+
+
+    if(accounts[from].withdraw(amount))
+    {
+        accounts[to].deposit(amount);
+
+        cout << "Transfer Successful";
+    }
+    else
+    {
+        cout << "Insufficient Balance";
+    }
 }
+
